bbb692edfb1cc9f291b7e1202167bf1676a28243
Nathan <dewittnathan@gmail.com>
2022-06-21 16:42:39 +0200

    Added practice tests

---
+++ PracticeProject/BasicGoogleTestWithoutFramework.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+using WebDriverManager.Helpers;
+using Xunit;
+
+namespace PracticeProject
+{
+    public class BasicGoogleTestWithoutFramework : IDisposable
+    {
+
+        public IWebDriver Driver { get; set; }
+
+        public BasicGoogleTestWithoutFramework()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig(), VersionResolveStrategy.MatchingBrowser);
+
+            Driver = new ChromeDriver();
+            Driver.Url = "https://www.google.co.za";
+            Driver.Manage().Window.Maximize();
+        }
+
+        [Fact]
+        public void BasicSearch()
+        {
+            var input = Driver.FindElement(By.CssSelector("input"));
+            input.SendKeys("This is a test");
+
+            var googleSearchButton = Driver.FindElement(By.CssSelector(".FPdoLc [value='Google Search']"));
+            googleSearchButton.Click();
+
+            Driver.FindElement(By.CssSelector(".hdtb-mitem.hdtb-msel > span"));
+        }
+
+        public void Dispose()
+        {
+            Driver.Quit();
+        }
+    }
+}
